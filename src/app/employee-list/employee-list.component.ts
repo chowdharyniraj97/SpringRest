38a75998533730de52d2f96898043dcaa3c159a6
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2021-05-10 12:37:05 -0400

    added employee component:

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Employee} from "../employee";
 
 @Component({
   selector: 'app-employee-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-
+  employees : Employee[];
   constructor() { }
 
   ngOnInit(): void {
