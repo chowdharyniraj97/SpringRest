0839d741d8ffa148dc266f38e6add224155f452a
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2021-05-10 13:15:42 -0400

    :added cors

--- src/main/java/com/mycompany/employee/controller/EmployeeController.java
+++ src/main/java/com/mycompany/employee/controller/EmployeeController.java
@@ -3,12 +3,13 @@ package com.mycompany.employee.controller;
 import com.mycompany.employee.model.Employee;
 import com.mycompany.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
