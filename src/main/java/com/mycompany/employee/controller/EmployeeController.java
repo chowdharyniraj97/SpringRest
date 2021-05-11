2852a79eed6df7997d02c949b2d25bb79691fcf2
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2021-05-11 17:03:36 -0400

    added api to get employee by ud

--- src/main/java/com/mycompany/employee/controller/EmployeeController.java
+++ src/main/java/com/mycompany/employee/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.mycompany.employee.controller;
 
+import com.mycompany.employee.exception.ResourceNotFoundException;
 import com.mycompany.employee.model.Employee;
 import com.mycompany.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +27,11 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee e =  employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee with " +id+" not found"));
+        return ResponseEntity.ok(e);
     }
 }
