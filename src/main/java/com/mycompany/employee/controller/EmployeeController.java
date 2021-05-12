71462fad6edd00d42f03e754d806e070c6bbf814
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2021-05-12 19:26:03 -0400

    added delete endpoint

--- src/main/java/com/mycompany/employee/controller/EmployeeController.java
+++ src/main/java/com/mycompany/employee/controller/EmployeeController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
@@ -44,5 +47,15 @@ public class EmployeeController {
         e.setLastName(emp.getLastName());
         Employee updatedEmployee = employeeRepository.save(e);
         return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable long id) {
+        Employee e =  employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee with " +id+" not found"));
+        employeeRepository.delete(e);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", true);
+        return ResponseEntity.ok(response);
+
     }
 }
